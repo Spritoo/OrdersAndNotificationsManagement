36ada01d9840893163d4cd5fe87a97489569441d
AliMoussa13 <a.a.m.tahon@gmail.com>
2023-12-24 16:27:12 +0200

    added all product related classes and functions

---
+++ src/main/java/Assignment2/OrdersAndNotificationsManagement/DB/ProductDB.java
@@ -0,0 +1,10 @@
+package Assignment2.OrdersAndNotificationsManagement.DB;
+
+import Assignment2.OrdersAndNotificationsManagement.model.Product;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProductDB {
+    public static Map<Integer, Product> products = new HashMap<Integer, Product>();
+}
--- src/main/java/Assignment2/OrdersAndNotificationsManagement/model/Product.java
+++ src/main/java/Assignment2/OrdersAndNotificationsManagement/model/Product.java
@@ -1,13 +1,13 @@
 package Assignment2.OrdersAndNotificationsManagement.model;
 
 public class Product {
-    String SerialNumber;
+    int SerialNumber;
     String Name;
     String Vendor;
     String Category;
-    float Price;
+    double Price;
     int Count;
-    public Product(String serial, String name, String vendor, String category, float price, int count) {
+    public Product(int serial, String name, String vendor, String category, double price, int count) {
         SerialNumber = serial;
         Name = name;
         Vendor = vendor;
@@ -23,5 +23,8 @@ public class Product {
     }
     public int getCount() {
         return Count;
+    }
+    public int getSerialNumber() {
+        return SerialNumber;
     }
 }
