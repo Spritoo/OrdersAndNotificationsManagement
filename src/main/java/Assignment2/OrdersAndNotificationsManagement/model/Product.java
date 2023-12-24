db530be55ed2b3e37450364aca3c159ec8115b3c
AliMoussa13 <a.a.m.tahon@gmail.com>
2023-12-24 15:16:55 +0200

    changed class product

--- src/main/java/Assignment2/OrdersAndNotificationsManagement/model/Product.java
+++ src/main/java/Assignment2/OrdersAndNotificationsManagement/model/Product.java
@@ -1,13 +1,27 @@
 package Assignment2.OrdersAndNotificationsManagement.model;
 
 public class Product {
-    String serialNumber;
-    String name;
+    String SerialNumber;
+    String Name;
     String Vendor;
-    String category;
-    float price;
-    int count;
-    public void printInfo(){
-        System.out.println("Product name: " + name + " Price: " + price);
+    String Category;
+    float Price;
+    int Count;
+    public Product(String serial, String name, String vendor, String category, float price, int count) {
+        SerialNumber = serial;
+        Name = name;
+        Vendor = vendor;
+        Category = category;
+        Price = price;
+        Count = count;
+    }
+    public void printInfo(){ // this shouldn't be here
+        System.out.println("Product name: " + Name + " Price: " + Price);
+    }
+    public void setCount(int count) {
+        Count = count;
+    }
+    public int getCount() {
+        return Count;
     }
 }
