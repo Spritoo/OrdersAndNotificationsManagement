99be25306dc89e1d149031d22f7473fe847e57f6
Pyrodash <18378976+Pyrodash@users.noreply.github.com>
2023-12-31 23:47:39 +0200

    Whatever

--- src/main/java/Assignment2/OrdersAndNotificationsManagement/controller/NotificationController.java
+++ src/main/java/Assignment2/OrdersAndNotificationsManagement/controller/NotificationController.java
@@ -1,8 +1,20 @@
 package Assignment2.OrdersAndNotificationsManagement.controller;
 
+import Assignment2.OrdersAndNotificationsManagement.model.notification.Notification;
+import Assignment2.OrdersAndNotificationsManagement.repository.classes.NotificationRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class NotificationController {
+import java.util.List;
 
+@RestController
+@RequestMapping("/notifications")
+public class NotificationController {
+    private NotificationRepository notificationRepository = NotificationRepository.getInstance();
+
+    @GetMapping("/queue")
+    public List<Notification> getAll() {
+        return notificationRepository.getAll();
+    }
 }
