7c0b952b017e6ca2cac2da7d9870899053a8daaf
Pyrodash <18378976+Pyrodash@users.noreply.github.com>
2023-12-31 23:48:36 +0200

    Whatever

--- src/main/java/Assignment2/OrdersAndNotificationsManagement/repository/interfaces/INotificationService.java
+++ src/main/java/Assignment2/OrdersAndNotificationsManagement/repository/interfaces/INotificationService.java
@@ -1,4 +1,18 @@
 package Assignment2.OrdersAndNotificationsManagement.repository.interfaces;
 
+import Assignment2.OrdersAndNotificationsManagement.model.Language;
+import Assignment2.OrdersAndNotificationsManagement.model.notification.Notification;
+
+import java.util.List;
+import java.util.Map;
+
 public interface INotificationService {
+    public boolean sendNotification(
+            Class<?> templateId,
+            Map<String, String> parameters,
+            String recipient,
+            Language language
+    );
+
+    public List<Notification> getAll();
 }
